ffae8038cff6ca270bc82c909db716f4aaaf214d
Log-zero <rlaalstjd423@naver.com>
2025-12-06 22:35:28 +0900

    fix: jekyll Gemfile for github actions

--- _plugins/callouts.rb
+++ _plugins/callouts.rb
@@ -1,57 +1,18 @@
-require 'kramdown'
+Jekyll::Hooks.register :documents, :post_convert do |doc|
+  content = doc.output
 
-module Jekyll
-  module CalloutConverter
-    CALLTOUT_REGEX = /^> \[!(\w+)\]\s*(.*)$/
+  content.gsub!(/<p>\[!(\w+)\]\s*(.*?)<\/p>/i) do
+    type = $1.downcase
+    title = $2.strip
 
-    def convert(content)
-      lines = content.lines
-      output = []
-      i = 0
-
-      while i < lines.length
-        line = lines[i]
-
-        # Callout 시작 찾기
-        if m = line.match(CALLTOUT_REGEX)
-          type = m[1].downcase
-          title = m[2].strip
-
-          body_lines = []
-          i += 1
-
-          # 블록이 끝날 때까지 body 수집
-          while i < lines.length && lines[i].start_with?('> ')
-            body_lines << lines[i].sub(/^> /, '')
-            i += 1
-          end
-
-          body = body_lines.join
-
-          html = %(
-            <div class="callout" data-callout="#{type}">
-              <div class="callout-title">#{title}</div>
-              <div class="callout-content">
-                #{Kramdown::Document.new(body).to_html}
-              </div>
-            </div>
-          )
-
-          output << html
-
-        else
-          output << line
-          i += 1
-        end
-      end
-
-      output.join
-    end
+    %Q(
+<div class="callout" data-callout="#{type}">
+  <div class="callout-title"><span class="callout-title-inner">#{title}</span></div>
+)
   end
 
-  Hooks.register :posts, :post_render do |post|
-    next unless post.output_ext == '.html'
-    converter = Object.new.extend(CalloutConverter)
-    post.output = converter.convert(post.output)
-  end
+  # callout 끝을 자동으로 닫기
+  content.gsub!(/(<\/div>\s*)?\z/, "</div>\n</div>")
+
+  doc.output = content
 end
