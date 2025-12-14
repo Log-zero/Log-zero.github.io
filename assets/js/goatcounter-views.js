b296bb6a2cb1494f0e7b26a06190c8d338115731
Log-zero <rlaalstjd423@naver.com>
2025-12-14 11:07:09 +0900

    visits

--- assets/js/goatcounter-views.js
+++ assets/js/goatcounter-views.js
@@ -1,13 +1,29 @@
 (function () {
   const site = "https://rlaalstjd423.goatcounter.com";
-  const path = location.pathname;
-  const url = site + "/counter/" + encodeURIComponent(path) + ".json";
 
-  fetch(url)
-    .then(res => res.json())
-    .then(data => {
-      const el = document.getElementById("goatcounter-views");
-      if (el) el.textContent = data.count || 0;
-    });
+  function normalizePath(path) {
+    if (!path.endsWith("/")) {
+      return path + "/";
+    }
+    return path;
+  }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const path = normalizePath(location.pathname);
+    const url = site + "/counter/" + encodeURIComponent(path) + ".json";
+
+    fetch(url)
+      .then(res => {
+        if (!res.ok) throw new Error("No counter");
+        return res.json();
+      })
+      .then(data => {
+        const el = document.getElementById("goatcounter-views");
+        if (el) el.textContent = data.count ?? 0;
+      })
+      .catch(() => {
+        const el = document.getElementById("goatcounter-views");
+        if (el) el.textContent = "0";
+      });
+  });
 })();
-
